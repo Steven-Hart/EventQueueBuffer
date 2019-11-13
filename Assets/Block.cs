eeec5ac18c58c756dc75a2ccc4611c70ff11c193
Steven-Hart <stevenhart11@gmail.com>
2019-11-13 21:18:25 +1100

    Buffer Puzzle Init

---
+++ Assets/Block.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class Block : MonoBehaviour
+{
+
+    public Vector2Int coord;
+
+    public event System.Action<Block> OnBlockPressed;
+    public event System.Action OnFinishedMoving;
+
+    public void Init(Vector2Int startingCoord, Texture2D image)
+    {
+        coord = startingCoord;
+        GetComponent<MeshRenderer>().material.shader = Shader.Find("Unlit/Texture");
+        GetComponent<MeshRenderer>().material.mainTexture = image;
+    }
+
+    public void MoveToPosition(Vector2 target, float duration)
+    {
+        StartCoroutine(AnimateMove(target, duration));
+    }
+    private void OnMouseDown()
+    {
+        if (OnBlockPressed != null)
+        {
+            OnBlockPressed(this);
+        }
+    }
+
+    IEnumerator AnimateMove(Vector2 target, float duration)
+    {
+        Vector2 initalPos = transform.position;
+        float percent = 0;
+
+        while (percent < 1)
+        {
+            percent += Time.deltaTime / duration;
+            transform.position = Vector2.Lerp(initalPos, target, percent);
+            yield return null;
+        }
+
+        if (OnFinishedMoving != null)
+        {
+            OnFinishedMoving();
+        }
+    }
+}
---
+++ Assets/Puzzle.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+public class Puzzle : MonoBehaviour
+{
+    public Texture2D image;
+    public int blocksPerLine = 4;
+    public int suffleLength = 20;
+    public float defaultMoveDuration = .2f;
+    public float shuffleMoveDuration = .1f;
+    
+    private Block emptyBlock;
+    private Block[,] blocks;
+    private bool blockIsMoving;
+    private Queue<Block> inputs;
+    private int suffleMovesRemaining;
+    private Vector2Int prevSuffleOffset;
+
+    private void Start()
+    {
+        CreatePuzzle();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartSuffle();
+        }
+        
+    }
+
+    void CreatePuzzle()
+    {
+        blocks = new Block[blocksPerLine,blocksPerLine];
+        Texture2D[,] imageSlices = ImageSlicer.GetSlices(image, blocksPerLine);
+        for (int y = 0; y < blocksPerLine; y++)
+        {
+            for (int x = 0; x < blocksPerLine; x++)
+            {
+                GameObject blockObject = GameObject.CreatePrimitive((PrimitiveType.Quad));
+                blockObject.transform.position = -Vector2.one * (blocksPerLine - 1) * 0.5f + new Vector2(x, y);
+                blockObject.transform.parent = transform;
+
+                Block block = blockObject.AddComponent<Block>();
+                block.OnBlockPressed += PlayerMoveBlockInput;
+                block.OnFinishedMoving += OnBlockFinishedMoving;
+                block.Init(new Vector2Int(x,y), imageSlices[x,y]);
+                blocks[x, y] = block;
+
+                if (y == 0 && x == blocksPerLine - 1)
+                {
+                    blockObject.SetActive(false);
+                    emptyBlock = block;
+                }
+            }
+        }
+
+        Camera.main.orthographicSize = blocksPerLine * .55f;
+        inputs = new Queue<Block>();
+    }
+
+    void PlayerMoveBlockInput(Block blockToMove)
+    {
+        inputs.Enqueue(blockToMove);
+        MakeNextPlayerMove();
+    }
+
+    void MakeNextPlayerMove()
+    {
+        while(inputs.Count > 0 && !blockIsMoving)
+        {
+            MoveBlock(inputs.Dequeue(), defaultMoveDuration);
+        }
+    }
+    
+    void MoveBlock(Block blockToMove, float duration)
+    {
+        if ((blockToMove.coord - emptyBlock.coord).sqrMagnitude == 1)
+        {
+            blocks[blockToMove.coord.x, blockToMove.coord.y] = emptyBlock;
+            blocks[emptyBlock.coord.x, emptyBlock.coord.y] = blockToMove;
+            
+            Vector2Int targetCoord = emptyBlock.coord;
+            emptyBlock.coord = blockToMove.coord;
+            blockToMove.coord = targetCoord;
+            
+            Vector2 targetPosition = emptyBlock.transform.position;
+            emptyBlock.transform.position = blockToMove.transform.position;
+            blockToMove.MoveToPosition(targetPosition, duration);
+            blockIsMoving = true;
+        }
+    }
+
+    void OnBlockFinishedMoving()
+    {
+        blockIsMoving = false;
+        MakeNextPlayerMove();
+
+        if (suffleMovesRemaining > 0)
+        {
+            MakeNextSuffleMove();
+        }
+    }
+
+    void StartSuffle()
+    {
+        suffleMovesRemaining = suffleLength;
+        MakeNextSuffleMove();
+    }
+
+    void MakeNextSuffleMove()
+    {
+        Vector2Int[] offsets =
+            {new Vector2Int(1, 0), new Vector2Int(-1, 0), new Vector2Int(0, 1), new Vector2Int(0, -1)};
+        int randomIndex = Random.Range(0, offsets.Length);
+
+        for (int i = 0; i < offsets.Length; i++)
+        {
+            Vector2Int offset = offsets[(randomIndex + i) % offsets.Length];
+            if (offset != prevSuffleOffset * -1)
+            {
+                Vector2Int moveBlockCoord = emptyBlock.coord + offset;
+
+                if (moveBlockCoord.x >= 0 && moveBlockCoord.x < blocksPerLine && moveBlockCoord.y >= 0 &&
+                    moveBlockCoord.y < blocksPerLine)
+                {
+                    MoveBlock(blocks[moveBlockCoord.x, moveBlockCoord.y], shuffleMoveDuration);
+                    suffleMovesRemaining--;
+                    prevSuffleOffset = offset;
+                    Debug.Log(suffleMovesRemaining);
+                    break;
+                }
+            }
+        }
+    }
+}
